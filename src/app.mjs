02419475286f55d11e6c1e69fcabb180f0f40d5c
Quillinan <milton.quillinan@gmail.com>
2023-07-02 00:19:32 -0300

    post participants

--- src/app.mjs
+++ src/app.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import "dotenv/config";
+import Joi from "joi";
 
 // Criação do app
 const app = express();
@@ -11,18 +12,54 @@ app.use(cors());
 app.use(express.json());
 
 // Conexão com o Banco
-const mongoClient = new MongoClient(process.env.DB_URL);
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 try {
-  await mongoClient.connect(); // top level await
-  console.log("MongoDB conectado!");
-
+	await mongoClient.connect() // top level await
+	console.log("MongoDB conectado!")
   app.listen(5000, () => {
     console.log('Servidor rodando na porta 5000');
   });
-
 } catch (err) {
-  console.log(err.message);
+	(err) => console.log(err.message)
 }
 
-const db = mongoClient.db();
+const db = mongoClient.db()
+
+// Rota POST /participants
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+
+  const schemaParticipant = Joi.object({
+    name: Joi.string().required()
+  });
+
+  const validation = schemaParticipant.validate(req.body, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map(detail => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  try {
+    const participant = await db.collection("participants").findOne({ name: name });
+    if (participant) return res.status(409).send("Nome já está sendo usado!");
+
+    const newParticipant = {
+      name,
+      lastStatus: Date.now()
+    };
+
+    await db.collection("participants").insertOne(newParticipant);
+    const successMessage = {
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: new Date().toLocaleTimeString("pt-BR", { hour: "2-digit", minute: "2-digit", second: "2-digit" })
+    };
+    return res.status(201).json(successMessage);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
